07592819efaedaf99c99e3bf1657ab0f04f9032b
Dzharenko <zharenko@mail.ru>
2025-06-23 22:38:40 +0300

    2 files and read JSON

---
+++ gendiff/parse_file.py
@@ -0,0 +1,10 @@
+import json
+
+def read_json(path_to_file):
+
+    return json.load(open(path_to_file))
+
+# path1 = 'files_directore/file1.json'
+# path2 = 'files_directore/file2.json'
+
+# print(open_json(path2))
