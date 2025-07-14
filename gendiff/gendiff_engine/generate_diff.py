5ed3da93930e5111dc94d857995ff550365eca13
Dzharenko <zharenko@mail.ru>
2025-07-14 23:50:39 +0300

    remake generate_diff, fix tests and do the coverage

--- gendiff/gendiff_engine/generate_diff.py
+++ gendiff/gendiff_engine/generate_diff.py
@@ -6,22 +6,24 @@ def generate_diff(file_path1, file_path2):
     dict1 = read_json(file_path1)
     dict2 = read_json(file_path2)
 
-    result = '' 
+    result = [] 
 
     diff_keys = dict1.keys() | dict2.keys()
     sort_keys = sorted(list(diff_keys))
-    
+
+
     for key in sort_keys:
         if key in dict1 and key in dict2:
             if dict1[key] == dict2[key]:
-                result += f" {key}: {dict1[key]} \n" 
+                result.append(f"    {key}: {dict1[key]}")
             else:
-                result += f"- {key}: {dict1[key]} \n" + \
-                          f"+ {key}: {dict2[key]} \n" 
+                result.append(f"  - {key}: {dict1[key]}")
+                result.append(f"  + {key}: {dict2[key]}") 
                                          
         if key in dict1 and key not in dict2:
-            result += f"- {key}: {dict1[key]} \n"
-        elif key not in dict1 and key in dict2:
-            result += f"- {key}: {dict2[key]} \n"
-
-    return "{ \n" + result + "}"
+            result.append(f"  - {key}: {dict1[key]}")
+        
+        if key not in dict1 and key in dict2:
+            result.append(f"  - {key}: {dict2[key]}")
+       
+    return "{\n" + '\n'.join(result) + "\n}"
